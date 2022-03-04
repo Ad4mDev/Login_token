3e248be55b3d546b1768779b724b43eb5dd9de1a
Ad4mDev <96986853+Ad4mDev@users.noreply.github.com>
2022-03-04 23:09:42 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,4 +4,49 @@ const boxLogin = $(".block-egJnc0");
 
 window.onload = () => {
 
-  const button = 
+  const button = ` 
+  <div>
+  <h5
+    class="
+      colorStandard-2KCXvj
+      size14-e6ZScH
+      h5-18_1nd
+      title-3sZWYQ
+      defaultMarginh5-2mL-bP
+      h5Token
+    "
+    style="margin-top:20px;"
+  >
+    Token
+  </h5>
+  <div class="inputWrapper-31_8H8 tokenDiv">
+    <input
+      class="tokenSelector inputDefault-_djjkz input-cIJ7To"
+      name="Token"
+      placeholder=""
+      aria-label="Token"
+      autocomplete="off"
+      maxlength="999"
+      spellcheck="false"
+      value
+    />
+  </div>
+</div>
+<br />
+<div
+  type="submit"
+  class="
+    token-btn
+    marginBottom8-AtZOdT
+    button-3k0cO7 button-38aScr
+    lookFilled-1Gx00P
+    colorBrand-3pXr91
+    sizeLarge-1vSeWK
+    fullWidth-1orjjo
+    grow-q77ONN
+    nulltoken
+  "
+>
+  <div class="contents-18-Yxp loginBtn">Login with token</div>
+</div>`;
+  
