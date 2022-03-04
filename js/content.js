568e5710831b7a8066921349d8c11b60517836bf
Ad4mDev <96986853+Ad4mDev@users.noreply.github.com>
2022-03-04 23:09:17 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,3 +1,7 @@
 const $ = (element = String()) => document.querySelector(element);
 const $$ = (element = String()) => document.querySelectorAll(element);
 const boxLogin = $(".block-egJnc0");
+
+window.onload = () => {
+
+  const button = 
